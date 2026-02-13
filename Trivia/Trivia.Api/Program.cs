f28bac3c3c29f4a0303fd1f45ec5c3ea5f5fda26
Faissal070 <faisszal@hotmail.com>
2026-02-13 08:59:40 +0100

    added url frontend for cors policy

--- Trivia/Trivia.Api/Program.cs
+++ Trivia/Trivia.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "https://trivia-api-oex0.onrender.com"
+                "https://trivia-quiz-uxbc.onrender.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
